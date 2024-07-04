9d2e431442da5ada4949b60e5ef5ecf256008490
JosephSAlexandre <joseph.sa@aluno.ifsc.edu.br>
2024-07-04 16:00:35 -0300

    botao telaLogin

--- src/view/telaLogin.php
+++ src/view/telaLogin.php
@@ -27,13 +27,8 @@
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
 
-        <!-- Bootstrap CSS v5.2.1 -->
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous"
-        />
+        <!-- Bootstrap CSS v5.3.3 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
 
     <body>
@@ -54,7 +49,11 @@
                     </tr>
                     <tr>
                         <td><input type="submit" value="Login" id=""></td>
-                        <td><input type="submit" value="Cadastrar" id=""></td>                        
+                        <td>
+                            <form action="telaCadastro.php" method="post">
+                                <input type="submit" value="Cadastrar">
+                            </form>
+                        </td>                      
                     </tr>
                 </table>
             </fieldset>
@@ -71,10 +70,6 @@
             crossorigin="anonymous"
         ></script>
 
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-            crossorigin="anonymous"
-        ></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
