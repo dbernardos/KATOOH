2f061c25bb41d9c9cef0d1d830d6c9bb6abdb9c1
Megwazi <rabello.lucas@outlook.com>
2024-07-04 15:20:50 -0300

    Tela Principal

---
+++ src/view/telaPrincipal.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"> 
+        <link rel="stylesheet" href="src\view\styles\styles.css"> 
+
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Katooh</a>
+              
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav" style="margin-left: 120px;">
+                    
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-house"></i> Início</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="bi bi-plus-square"></i> Criar uma sala</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="bi bi-arrow-right-square"></i> Entrar em uma sala</a>
+                  </li>
+                  <div class="d-flex flex-row-reverse bd-highlight">
+                    <li class="nav-item d-flex flex-row-reverse bd-highlight">
+                        <a class="nav-link" href="#"><i class="bi bi-person"></i> Gerenciar Conta</a>
+                      </li>
+                  </div>
+                </ul>
+              </div>
+            
+            </div>
+          </nav>
+    </body>
+</html>
