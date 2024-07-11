43f7633204f714afd745a430e9213a7e7426ae14
Megwazi <rabello.lucas@outlook.com>
2024-07-11 15:13:40 -0300

    implementação styles

--- src/view/telaCadastro.php
+++ src/view/telaCadastro.php
@@ -4,14 +4,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link href="src/view/styles/styles.css" rel="stylesheet">
+        <link href="../src/view/styles/styles.css" rel="stylesheet">
         <title>Cadastro KATOOH</title>
-        <style>
-            
-        </style>
+        
     </head>
 
-    <header>
+    <header class="oval-header">
+        <div class="oval-bottom"></div>
         <div class="row">
             <div class="colunaImagem">
                 <img class="logo" src="../view/img/K.svg">
